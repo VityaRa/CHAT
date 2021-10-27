5fad87c6e657e8d3ef7a6bc9234ebc72646258fc
VityaRa <blackops2in@mail.ru>
2021-10-28 01:55:01 +0300

    feat: add extra types, enums, constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,40 @@
+import { IMessage } from "./types";
+
+export const messages: IMessage[] = [
+    {
+        name: "Victor",
+        userId: "1",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+    {
+        name: "Victor",
+        userId: "2",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+    {
+        name: "Victor",
+        userId: "1",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+    {
+        name: "Victor",
+        userId: "1",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+    {
+        name: "Victor",
+        userId: "1",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+    {
+        name: "Victor",
+        userId: "1",
+        message: "Hello!",
+        date: "23-01-2002",
+    },
+]
---
+++ src/utils/types.ts
@@ -0,0 +1,6 @@
+export interface IMessage {
+    name: string,
+    userId: string,
+    message: string,
+    date: string,
+}
